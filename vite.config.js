76be011097922bb5a1bc1ea21068896a8e0fdba6
Ahmed-abulfateh <ahmed-bh91@live.com>
2026-05-07 09:29:46 +0300

    Add vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+// https://vite.dev/config/
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            output: {
+                entryFileNames: 'assets/index-fC-vG223.js',
+                chunkFileNames: 'assets/[name].js',
+                assetFileNames: (assetInfo) => {
+                    if (assetInfo.name === 'style.css' || assetInfo.name?.endsWith('.css')) {
+                        return 'assets/index-rBjRCZA4.css';
+                    }
+
+                    return 'assets/[name][extname]';
+                },
+            },
+        },
+    },
+    server: {
+        host: '127.0.0.1',
+        port: 5173,
+        proxy: {
+            '/api': 'http://localhost:3000',
+        },
+    },
+    plugins: [react()],
+});
